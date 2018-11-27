670cfaad39cd8a0480794fd4655687b5ddea5a26
elias.eckermann <elias.eckermann@KK-GYM-EDV1-15.gym.kk.local>
2018-11-27 17:39:44 +0100

    Kommentare hinzugefügt

--- HelloWorldProjekt/src/HelloWorld.java
+++ HelloWorldProjekt/src/HelloWorld.java
@@ -3,5 +3,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hallo WIN");
+		// das ist ein kommentar,kein codeder ausgeführt wird
+		// das ist ein einzeiliger Kommentar
+		/*
+		 * Mehrzeiliger Kommentar Schreiben
+		 */
 	}
 }
