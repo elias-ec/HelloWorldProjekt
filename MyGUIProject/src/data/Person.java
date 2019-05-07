052d19d1fa3fe461b97a3878bf649801912e1063
elias.eckermann <elias.eckermann@KK-GYM-EDV1-15.gym.kk.local>
2019-05-07 18:03:02 +0200

    123

---
+++ MyGUIProject/src/data/Person.java
@@ -0,0 +1,46 @@
+package data;
+
+public class Person {
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public String getNachname() {
+		return nachname;
+	}
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+	public String getStrasse() {
+		return strasse;
+	}
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
+	}
+	public String getHausnummer() {
+		return hausnummer;
+	}
+	public void setHausnummer(String hausnummer) {
+		this.hausnummer = hausnummer;
+	}
+	public String getWohnort() {
+		return wohnort;
+	}
+	public void setWohnort(String wohnort) {
+		this.wohnort = wohnort;
+	}
+	public String getPlz() {
+		return plz;
+	}
+	public void setPlz(String plz) {
+		this.plz = plz;
+	}
+	private String vorname;
+	private String nachname;
+	private String strasse;
+	private String hausnummer;
+	private String wohnort;
+	private String plz;
+}
---
+++ MyGUIProject/src/test/PersonTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import data.Person;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String personText;
+		Person p1; //Variablen Definition
+		p1 = new Person(); //Instanzierung
+//
+		p1.setVorname("Hans");
+		p1.setNachname("Huber");
+		//
+		System.out.println(p1);
+	}
+
+}
